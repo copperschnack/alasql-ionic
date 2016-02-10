1f3e3bf3f7b9d054807de509f83df6526abb4140
nbdamian <damian@ninetyblack.com>
2016-02-11 10:13:31 +1300

    Ionic Build & alasql

---
+++ platforms/android/build/intermediates/assets/debug/www/js/app.js
@@ -0,0 +1,55 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+
+	alasql('CREATE FILESTORAGE DATABASE test1("./test1.json")');
+	alasql('ATTACH FILESTORAGE DATABASE test1("./test1.json")');
+	alasql('USE test1');
+	alasql("CREATE TABLE IF NOT EXISTS products (id INT, category_id INT, name string, created_at DATE)");
+	alasql("INSERT INTO products (category_id, name, created_at) VALUES (?,?,?,?)", [1, 2, 'XYZ', new Date()]);
+
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+  .state('dash', {
+    url: '/dash',
+    views: {
+      'dash': {
+        templateUrl: 'templates/dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+});
